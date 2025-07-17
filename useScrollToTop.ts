ff2640c0ca4bb5ae9b2ddd157ba34e0c6a609da5
Nyxveilsmith <147945162+nyxveilsmith@users.noreply.github.com>
2025-07-17 09:22:31 +0400

    Update useScrollToTop.ts

--- useScrollToTop.ts
+++ useScrollToTop.ts
@@ -1,10 +1,13 @@
-import { useEffect } from 'react';
-import { useLocation } from 'wouter';
+import { useEffect } from "react";
+import { useLocation } from "wouter";
 
 export const useScrollToTop = () => {
   const [location] = useLocation();
-  
+
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // Delay for smooth rendering
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, 0);
   }, [location]);
 };
